b54bc0514bd55c0dcaba1a54c7b2d9e617d239af
juanm4morales <juanm4morales@gmail.com>
2024-05-12 22:58:56 -0300

    config: mapper config file, rviz_slam file added, lidar: increased lidar range to 20, urdf core: added base_footprint_link for mapping purpose, launch_sim splitted into launch_sim_joy and launch_sim (without joystick launch).

---
+++ src/boxbots/launch/launch_sim_slam.launch.py
@@ -0,0 +1,33 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+
+    package_name='boxbots'
+
+    sim = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','launch_sim.launch.py'
+                )]),
+    ) 
+    slam = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("slam_toolbox"),'launch','online_async_launch.py'
+                )]), launch_arguments={'params_file': './src/boxbots/config/mapper_params_online_async.yaml',
+                                       'use_sim_time': 'true'}.items()
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        sim,
+        slam     
+    ])
