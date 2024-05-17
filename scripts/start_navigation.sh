73160a13e7d8ac69197d737b9f9b9d7453864dcb
Nano-Nathan <nanonathan.123@gmail.com>
2024-05-17 20:00:08 -0300

    edito script para inciar navegación

--- scripts/start_navigation.sh
+++ scripts/start_navigation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "ros2 run rviz2 rviz2 -d src/boxbots/config/rviz_slam_with_navigation.rviz; exec bash"
-ros2 launch boxbots navigation_launch.launch.py; exec bash
+gnome-terminal -- bash -c "ros2 launch boxbots navigation_launch.launch.py"
+ros2 run rviz2 rviz2 -d ./../src/boxbots/config/rviz_slam_with_navigation.rviz
