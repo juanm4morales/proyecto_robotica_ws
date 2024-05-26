331550ee00e2cef31e64da90f57cc6ea04e05640
Carlos Perez <53824547+CarlosPerezVE@users.noreply.github.com>
2024-05-26 17:37:51 -0300

    Delete scripts directory

--- scripts/run_docker.bat
+++
@@ -1,9 +0,0 @@
-
-
-docker run -it \
-  --name=r2_boxbots_container \
-  -e DISPLAY=host.docker.internal:0.0 \
-  -e LIBGL_ALWAYS_INDIRECT=0 \
-  --volume="${PWD}/../:/root/project" \
-  r2_boxbots \
-  bash
